a9b12487e68a4424e3b38a831b091b8ae3d6695f
Lluis <lluiso.aloha@gmail.com>
2019-09-22 14:52:53 +0200

    Set up Materialize CSS. Added a simple carousel.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-materialize';
+export class AppComponent implements AfterViewInit {
+  title = 'Materialize Angular';
+  @ViewChild('carousel', {static: false}) carousel: ElementRef;
+  constructor() {}
+  ngAfterViewInit() {
+    const options = {
+      duration: 300,
+      dist: 0,
+      shift: 30
+    }
+    M.Carousel.init(this.carousel.nativeElement, options);
+  }
 }
